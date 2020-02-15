f3d035d5efd8b3c9d1ca4bf248b6cad499585ada
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 03:23:38 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,13 @@
 def unsafe?(speed)
-  if speed > 79 
-  "true"
- elsif
-  "true"
+  if speed < 40 
+  false
+  else
+  true
   end
 end
 
 
 
-def not_safe(speed)
-	speed < 40 ? "false" : "true"
+def not_safe?(speed)
+	speed < 40 ? false : true
 end
