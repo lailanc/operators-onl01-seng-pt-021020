3a54f9aa7920ab08fd12aca787efe3dc168b7e8d
Laila Campbell <lailacampbell96@gmail.com>
2020-02-15 03:12:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,26 +1,13 @@
- 
 def unsafe?(speed)
-puts 
-  
-
-  
-
-
-   
+  if speed < 40
+    "safe"
+  else speed > 60
+    "unsafe"
+  end
 end
-
-
-
 
 
 
 def not_safe?(speed)
-  
-
-	
+	speed < 40 ? "safe" : "unsafe"
 end
-
-
-	
-
-
